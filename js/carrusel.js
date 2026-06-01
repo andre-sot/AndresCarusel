bf4400ebde4982317b7d548e23c09142293fc24d
andre-sot <20253ds023@utez.edu.mx>
2026-06-01 10:34:46 -0600

    andres

---
+++ js/carrusel.js
@@ -0,0 +1,19 @@
+
+var imagenes =[
+    "img/img1.jpg",
+    "img/img2.jpg",
+    "img/img3.jpg",
+    "img/img4.jpg",
+    "img/img5.jpg",
+]
+var i = 0;
+
+document.getElementById("siguiente").addEventListener("click",()=> {
+    if(i < 4){i++;}
+    document.getElementById("img").setAttribute("src",imagenes[i])
+});
+
+document.getElementById("anterior").addEventListener("click",()=> {
+    if(i > 0){i++;}
+    document.getElementById("img").setAttribute("src",imagenes[i]);
+});
